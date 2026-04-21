1303cfe3521b4c4d9cdd1012e90ade7e49e2cde1
ShreyaDeshmukh13 <shreyadesh0103@gmail.com>
2026-04-21 08:48:41 +0530

    Time: 0 ms (100%), Space: 80.4 MB (24.32%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+         int start = 0 ;
+    int end = arr.length - 1 ;
+    while(start < end)
+    {
+        int mid = start + (end - start) / 2;
+        if(arr[mid] >  arr[mid + 1])
+        {
+            //you are in decreasing part of array
+            // this may be the ans but look at left
+            end = mid ;
+        }
+        else{
+            // you are in asc part of MountainArray
+            start = mid + 1 ; // becuase we know that mid + 1 > mid , hence ignored mid
+        }
+    }
+ // in the end, start == end and pointing to the largest number because of the 2
+// start and end are always trying to find max element in the above 2 checks
+// hence if they're pointing to just one element , it is the maximum one
+return start ; // or end as both are equal 
+
+        
+    }
+}
