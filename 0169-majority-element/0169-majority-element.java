c4dc678459ad7a43b48ab336071122e6049abb93
Shreya Deshmukh <shreyadesh0103@gmail.com>
2026-08-05 16:55:53 +0530

    Time: 1923 ms (5.12%), Space: 55.5 MB (66.7%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,27 +1,23 @@
 class Solution {
     public int majorityElement(int[] arr) {
-         int n = arr.length; 
-
-        // Loop to consider each element as 
-        // a candidate for majority
-        for (int i = 0; i < n; i++) {
+        int n = arr.length ; 
+        
+        for(int i = 0 ; i < n ; i++)
+        {
             int count = 0;
-
-            // Inner loop to count the frequency of arr[i]
-            for (int j = 0; j < n; j++) {
-                if (arr[i] == arr[j]) {
-                    count++;
+            for(int j = 0 ; j < n ; j++)
+            {
+                if(arr[i] == arr[j])
+                {
+                    count++ ;
                 }
             }
-
-            // Check if count of arr[i] is more
-            // than half the size of the array
-            if (count > n / 2) {
-                return arr[i];
-            }
+        
+        if(count > n/2)
+        {
+            return arr[i] ;
         }
-
-        // If no majority element found, return -1
-        return -1;
+        }
+    return -1;
     }
 }
